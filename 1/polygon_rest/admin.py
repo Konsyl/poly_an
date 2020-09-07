8ca6db11d79ef0a0f3b5a646b89546e7072bfbe1
Konsylat <Konsylckat@gmail.com>
2020-09-07 23:45:56 +0300

    fix arc

--- 1/polygon_rest/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from polygon_rest.models import Dot
-from polygon_rest.models import Rectangle, FRectangle
-
-
-admin.site.register(Dot)
-admin.site.register(Rectangle)
-admin.site.register(FRectangle)
--- 1/polygon_rest/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PolygonRestConfig(AppConfig):
-    name = 'polygon_rest'
